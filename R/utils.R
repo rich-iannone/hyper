18bf5524af60d9a9d7a14f4518c1f1981288d6fd
rich-iannone <riannone@me.com>
2018-02-15 13:56:51 -0500

    Add internal 'supplied_components_tbl' fcn

---
+++ R/utils.R
@@ -0,0 +1,19 @@
+
+
+# Create a table based on an input list; this
+# is to detect whether there is the main data
+# object somewhere in the list
+supplied_components_tbl <- function(object_list) {
+
+  seq(object_list) %>%
+    purrr::map_df(
+      .f = function(x) {
+        dplyr::tibble(
+          list_item = x,
+          is_object = ifelse(
+            inherits(object_list[x], "list") &
+              object_list[[x]] %>% rlang::has_name(name = "name") &
+              object_list[[x]] %>% rlang::has_name(name = "stmts"),
+            TRUE, FALSE))})
+}
+
