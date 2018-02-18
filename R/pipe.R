2539e1701c50ecea1608dcbc2275990895b2e999
rich-iannone <riannone@me.com>
2018-02-17 22:49:15 -0500

    Add magrittr pipe

---
+++ R/pipe.R
@@ -0,0 +1,10 @@
+#' The magrittr pipe
+#'
+#' hyper uses the pipe function [%>%] to turn
+#' function composition into a series of
+#' imperative statements
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
+#' @export
+NULL
