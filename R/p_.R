898c63ed1b40e37b9129634534001274386076d6
rich-iannone <riannone@me.com>
2018-02-19 01:35:15 -0500

    Use generalized fcns to better construct element

--- R/p_.R
+++ R/p_.R
@@ -8,7 +8,9 @@
 #' @importFrom glue glue
 #' @importFrom rlang prepend
 #' @export
-p_ <- function(...) {
+p_ <- function(...,
+               id = NULL,
+               class = NULL) {
 
   # Define main attributes of constructor
   type <- "p"
@@ -17,76 +19,63 @@ p_ <- function(...) {
   # Gather list of input data
   x_in <- list(...)
 
-  attrs_spacer <- ifelse(has_attr_components(x_in), " ", "")
+  # Generate `id` statement
+  id_statement <- generate_id_stmt(id)
 
-  x_attr <- get_attr_components(x_in) %>% paste(collapse = " ")
-  x_text <- get_text_components(x_in) %>% paste(collapse = " ")
+  # Generate `class` statement
+  class_statement <- generate_class_stmt(class)
 
-  open_tag <-
-    glue::glue("<{type}{attrs_spacer}{x_attr}>") %>%
-    as.character() %>%
-    trimws()
+  # Collect all opening tag attrs
+  tag_attrs <-
+    collect_all_attrs(
+      id_statement,
+      class_statement,
+      get_attr_components(x_in))
 
-  close_tag <-
-    glue::glue("</{type}>") %>%
-    as.character()
+  # Create the opening tag
+  opening_tag <-
+    create_opening_tag(
+      type = type,
+      attrs_str = tag_attrs)
+
+  # Create the closing tag
+  closing_tag <-
+    create_closing_tag(type = type)
+
+  # Generate the content for the tag
+  content <-
+    get_text_components(x_in) %>%
+    paste(collapse = " ") %>%
+    commonmark::markdown_html() %>%
+    strip_outer_tags("p") %>%
+    remove_trailing_linebreaks()
+
+  # Generate the complete HTML element
+  html_element <-
+    create_html_element(
+      opening_tag = opening_tag,
+      closing_tag = closing_tag,
+      content = content)
 
   # Get the input components to the function
   input_component_list <-
     get_input_component_list(input_list = x_in)
 
-  # Case where there are no input components
-  if (input_component_list$input_component_count == 0) {
+  # Case where there is no input object
+  if (input_component_list$input_object_count == 0) {
 
     x_out <-
       initialize_object(
         type = type,
         mode = mode,
-        text = glue::glue("{open_tag}{close_tag}") %>%
-          as.character())
-
-    return(x_out)
-  }
-
-  # Case where there are no input objects
-  # but some input components
-  if (input_component_list$input_object_count == 0 &
-      input_component_list$input_component_count > 0) {
-
-    # Handle text components
-    x_text <-
-      x_text %>%
-      paste(collapse = " ") %>%
-      commonmark::markdown_html() %>%
-      strip_outer_tags("p") %>%
-      remove_trailing_linebreaks() %>%
-      rlang::prepend(open_tag) %>%
-      append(close_tag) %>%
-      paste(collapse = "")
-
-    x_out <-
-      initialize_object(
-        type = type,
-        mode = mode,
-        text = x_text)
+        text = html_element)
 
     return(x_out)
   }
 
   # Case where there is an input object
-  # and possibly some input components
   if (input_component_list$input_object_count == 1 &
       input_component_list$input_contains_obj_x) {
-
-    x_text <-
-      x_text %>%
-      paste(collapse = " ") %>%
-      commonmark::markdown_html() %>%
-      strip_outer_tags("p") %>%
-      remove_trailing_linebreaks() %>%
-      rlang::prepend(open_tag) %>%
-      append(close_tag) %>%
-      paste(collapse = "")
 
     input_component_x <- x_in[[1]]
 
@@ -94,7 +83,7 @@ p_ <- function(...) {
       initialize_object(
         type = type,
         mode = mode,
-        text = x_text)
+        text = html_element)
 
     x_out <-
       list(
