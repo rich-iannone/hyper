40a0cf8363b95ea6d937311c7ab2d1212869ccfe
rich-iannone <riannone@me.com>
2018-02-20 02:15:39 -0500

    Modify roxygen @param statements

--- R/global.R
+++ R/global.R
@@ -6,42 +6,44 @@
 #' @param style an inline CSS style for the element.
 #' @param title used for extra information about the element.
 #' @param lang the language for the element's content.
-#' @param hidden when [TRUE], this states that the element is
-#' not yet, or perhaps no longer, relevant.
+#' @param hidden when \code{TRUE}, this states that the
+#' element is not yet, or perhaps no longer, relevant.
 #' @param data this stores custom data private to the page
 #' or animation.
 #' @param accesskey specifies a shortcut key to
 #' activate/focus an element.
-#' @param contenteditable can either be [TRUE] or [FALSE] and
-#' this specifies whether the content of an element is
-#' editable or not.
+#' @param contenteditable can either be \code{TRUE} or
+#' \code{FALSE} and this specifies whether the content of
+#' an element is editable or not.
 #' @param contextmenu specifies a context menu for an element.
 #' The context menu appears when a user right-clicks on the
 #' element. The value of the contextmenu attribute is the id
-#' of the [<menu>] element to open.
+#' of the \code{<menu>} element to open.
 #' @param dir specifies the text direction of the element's
-#' content. Possible values are [ltr] (left to right)
-#' [rtl] (right to left), or [auto] (which leaves the browser
-#' to figure out the text direction).
+#' content. Possible values are \code{ltr} (left to right)
+#' \code{rtl} (right to left), or \code{auto} (which leaves
+#' the browser to figure out the text direction).
 #' @param draggable specifies whether an element is
-#' draggable or not. This can either be [TRUE], [FALSE], or
-#' [auto] (which uses the default behavior of the browser).
+#' draggable or not. This can either be \code{TRUE},
+#' \code{FALSE}, or \code{auto} (which uses the default
+#' behavior of the browser).
 #' @param dropzone specifies whether the dragged data is
 #' copied, moved, or linked, when it is dropped on an
 #' element. This attribute can have either of these values:
-#' [copy] (dropped data will result in a copy of the dragged
-#' data), [move] (dropped data will result in the movement
-#' of the dragged data to the new location), and [link]
-#' (dropped data will result in a link to the original data).
+#' \code{copy} (dropped data will result in a copy of the
+#' dragged data), \code{move} (dropped data will result
+#' in the movement of the dragged data to the new location),
+#' and \code{link} (dropped data will result in a link to the
+#' original data).
 #' @param spellcheck specifies whether the element is to
 #' have its spelling and grammar checked or not. The
 #' following can be spellchecked: (1) text values in
 #' input elements (not passwords though), (2) text in
-#' [<textarea>] elements, and (3) text in editable elements.
-#' This can either be [TRUE] or [FALSE].
+#' \code{<textarea>} elements, and (3) text in editable
+#' elements. This can either be \code{TRUE} or \code{FALSE}.
 #' @param translate specifies whether the content of an
 #' element should be translated or not. This can either be
-#' [yes] or [no].
+#' \code{yes} or \code{no}.
 #' @importFrom purrr map_chr
 #' @export
 
