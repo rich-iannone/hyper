41d111ec608ada00e7e76cf7d104888cdb3a127d
rich-iannone <riannone@me.com>
2018-02-22 03:03:33 -0500

    Add 'create_css_rules' function

---
+++ R/create_css_rules.R
@@ -0,0 +1,14 @@
+#' Create a set of CSS rules
+#'
+#' Create a set of CSS rules that can be added
+#' to an HTML document or written to a file.
+#' @param selectors the selector or set of selectors
+#' to use for the CSS rule set.
+#' @param rules a list of rules to use in the rule
+#' set's declaration block.
+#' @export
+create_css_rules <- function(selectors,
+                             rules = NULL) {
+
+  NULL
+}
